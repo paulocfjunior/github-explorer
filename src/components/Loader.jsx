02a7016ba4708a92edf0bb172812ca1e270fdd2e
Paulo Junior <paulo.junior@liferay.cloud>
2019-10-08 01:54:53 -0300

    Create Loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import "../styles/components/Loader.scss";
+import React from "react";
+
+function Loader() {
+  return (
+    <div className="Loader">
+      <span className="dot" />
+      <span className="dot" />
+      <span className="dot" />
+    </div>
+  );
+}
+
+export default Loader;
